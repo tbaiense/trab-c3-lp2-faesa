a7909f2b03cecf6d314d3c9ee72dce604cb6cc59
Gabriel Rodrigues <gabrielrcsj@gmail.com>
2026-06-04 23:39:59 -0300

    Adicionado as classes Admin, Atendente e Funcionario

---
+++ src/modelos/Admin.java
@@ -0,0 +1,8 @@
+package modelos;
+
+public class Admin extends Funcionario {
+	public static void main(String[] args) {
+		
+		
+	}
+}
---
+++ src/modelos/Atendente.java
@@ -0,0 +1,10 @@
+package modelos;
+
+public class Atendente extends Funcionario {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
