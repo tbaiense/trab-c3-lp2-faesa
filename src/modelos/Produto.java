66d3a4bbc8ee8883dd6e30da7cf30aad60f76a17
Lucas Rafalski Rodrigues <lucasrafalskirodrigues@gmail.com>
2026-06-11 20:07:24 -0300

    criacao de instancias, variaveis getters e setters para Produto e CatalogoProdutos

--- src/modelos/Produto.java
+++ src/modelos/Produto.java
@@ -1,5 +1,53 @@
 package modelos;
 
 public class Produto {
-	String a = "teste";
+	private int idProduto;
+	private String nomeProduto;
+	private double precoCusto;
+	private double precoVenda;
+
+	public Produto() {
+
+	}
+
+	public int getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(int idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public double getPrecoCusto() {
+		return precoCusto;
+	}
+
+	public void setPrecoCusto(double precoCusto) {
+		this.precoCusto = precoCusto;
+	}
+
+	public double getPrecoVenda() {
+		return precoVenda;
+	}
+
+	public void setPrecoVenda(double precoVenda) {
+		this.precoVenda = precoVenda;
+	}
+
+	public Produto(int idProduto, String nomeProduto, double precoCusto, double precoVenda) {
+		super();
+		this.idProduto = idProduto;
+		this.nomeProduto = nomeProduto;
+		this.precoCusto = precoCusto;
+		this.precoVenda = precoVenda;
+	}
+
 }
